50a1e57b0dd91e9602db09d739e58add2c16a4a1
notlec <elton.cheung@gmail.com>
2026-03-03 17:33:30 -0800

    debug

--- modules/constants.py
+++ modules/constants.py
@@ -1,6 +1,6 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 
 supabase_db_host = os.getenv("SUPABASE_DB_HOST_URL", "aws-0-us-west-2.pooler.supabase.com")
 supabase_db_password = os.getenv("SUPABASE_DB_PASSWORD")
