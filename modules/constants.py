455462d5c90e3442dfce0c2ce39b51efffafd3cf
notlec <elton.cheung@gmail.com>
2026-03-03 17:27:15 -0800

    added default host url

--- modules/constants.py
+++ modules/constants.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-supabase_db_host = os.getenv("SUPABASE_DB_HOST_URL")
+supabase_db_host = os.getenv("SUPABASE_DB_HOST_URL", "aws-0-us-west-2.pooler.supabase.com")
 supabase_db_password = os.getenv("SUPABASE_DB_PASSWORD")
 database_pool_size_str = os.getenv("DATABASE_POOL_SIZE")
 
